38f2493a90666d114ff575859ee3b02276cedbc7
cmansanarez <cameron.mansanarez@gmail.com>
2026-05-20 23:49:51 -0600

    Updates to index.html and file structure

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,12 @@
+(function() {
 "use strict";
 
-( () => {
+document.querySelector('#testButton').addEventListener('click', handleClick);
+
+function handleClick() {
+  console.log('Button was clicked!');
+}
 
 
-}) ();
+} ());
 
