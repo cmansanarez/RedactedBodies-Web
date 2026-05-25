fa9e3abb504040c0e160fadc79cd5372aa356353
cmansanarez <cameron.mansanarez@gmail.com>
2026-05-24 22:31:35 -0600

    Update to navbar scroll effect; solid upon scroll listener

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,15 @@
 (function() {
 "use strict";
 
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
 
 } ());
 
