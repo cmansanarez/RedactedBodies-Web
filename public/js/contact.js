f648b22c72bf9819e63843529ad66c9808e28a09
cmansanarez <cameron.mansanarez@gmail.com>
2026-05-24 20:27:44 -0600

    Updates to styling form page and contact.js

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,17 +1,35 @@
 (function() {
 "use strict";
 
-document.querySelector("#contact-form-button").addEventListener("click", (event) => {
+const form = document.querySelector("#contact-form");
+
+form.addEventListener("submit", (event) => {
     event.preventDefault();
     event.stopPropagation();
-    console.log("You clicked the submit button");
-    let name = document.querySelector("#name").value;
-    let email = document.querySelector("#mail").value;
-    let message = document.querySelector("#msg").value;
-    console.log("Name: " + name);
-    console.log("Email: " + email);
-    console.log("Message: " + message);
+
+    let formValid = true;
+
+    if (!form.checkValidity()) {
+        formValid = false;
+    }
+
+    form.classList.add("was-validated");
+
+    if (formValid) {
+        sendEmail();
+    }
 });
 
-} ());
+function sendEmail() {
+    let name = document.querySelector("#name").value;
+    let email = document.querySelector("#mail").value;
+    let inquiryType = document.querySelector("#inquiry-type").value;
+    let message = document.querySelector("#msg").value;
 
+    console.log("Name: " + name);
+    console.log("Email: " + email);
+    console.log("Inquiry Type: " + inquiryType);
+    console.log("Message: " + message);
+}
+
+} ());
