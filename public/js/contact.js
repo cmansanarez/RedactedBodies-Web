18f3721ea22f510f64a6c164246948a6a21b0a91
cmansanarez <cameron.mansanarez@gmail.com>
2026-05-24 22:33:33 -0600

    Add navbar scroll listener in contact.js

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,6 +1,16 @@
 (function() {
 "use strict";
 
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
+
 const form = document.querySelector("#contact-form");
 
 form.addEventListener("submit", (event) => {
