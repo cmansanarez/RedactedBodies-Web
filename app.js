5ed0d53c32c3e64d72b8c0760fad766a3b3240aa
YOUR_NAME <YOUR_EMAIL>
2026-06-16 15:26:21 -0600

    Wired up the stub /projects and /project pages with actual data and routing logic. Complete the EJS foundations.

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.static('public'));
 
+const projects = [
+    { name: 'Lumyn: Constellation of Self', url: 'https://visit-lumyn.noirmak.com/', image: 'noirmak-visit-lumyn.jpg' },
+    { name: 'GLITCH.EXE: Orpheus Protocol', url: 'https://orpheus.noirmak.com/', image: 'noirmak-orpheus.jpg' },
+    { name: 'stillbecoming', url: 'https://stillbecoming.noirmak.com/', image: 'noirmak-still-becoming.jpg' },
+    { name: 'Undertones', url: 'https://undertones.noirmak.com/', image: 'noirmak-undertones.jpg' },
+    { name: 'Between States', url: 'https://between-states-pitch-deck.noirmak.com/', image: 'noirmak-between-states.jpg' },
+    { name: 'Noise Palette', url: 'https://noise-palette.noirmak.com/', image: 'noirmak-noise-palette.jpg' },
+    { name: 'Atmos Index', url: 'https://atmosindex.noirmak.com/', image: 'noirmak-atmos-index.jpg' },
+    { name: 'Stitched in Code', url: 'https://stitchedincode.noirmak.com/', image: 'noirmak-web-stitched-in-code.jpg' },
+    { name: 'Lumyn', url: 'https://lumyn.noirmak.com/', image: 'noirmak-lumyn-web.jpg' }
+];
+
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -25,11 +37,15 @@ app.get('/contact', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-    res.render('projects');
+    res.render('projects', { projectArray: projects });
 });
 
-app.get('/project', (req, res) => {
-    res.render('project');
+app.get('/project/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id < 1 || id > projects.length) {
+        throw new Error('No project with that ID');
+    }
+    res.render('project', { project: projects[id - 1], id: id });
 });
 
 app.get('/newProject', (req, res) => {
@@ -46,6 +62,10 @@ app.post('/mail', async (req, res) => {
     }
 });
 
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.render('error');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
